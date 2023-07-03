d1a793c8858105b84e2a82c7bcf7b5fcf83a4e01
GamalAbdELNasser77 <131398745+GamalAbdELNasser77@users.noreply.github.com>
2023-07-03 16:52:44 +0300

    gamal form login

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,14 +1,11 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-  
-
-
+  responsive() {}
 }
