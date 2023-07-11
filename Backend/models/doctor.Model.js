d4dcbd035d0ed6bb1f9c979e0152bc2256c852db
mina holouma <minaholouma@gmail.com>
2023-07-11 13:27:57 +0300

    update

--- Backend/models/doctor.Model.js
+++ Backend/models/doctor.Model.js
@@ -16,6 +16,38 @@ doctorSchema = new Schema({
 }
 )
 doctor = mongoose.model('Doctor', doctorSchema);
+const Doctor = mongoose.model('Doctor', doctorSchema);
+
+// API endpoint to handle form data submission
+app.post('https://asd-6gr1.onrender.com/doctors', (req, res) => {
+  const formData = req.body;
+
+  // Create a new Doctor document with the form data
+  const newDoctor = new Doctor({
+    name: formData.name,
+    image: formData.image,
+    clinicName: formData.clinicName,
+    location: formData.location,
+    email: formData.email,
+    phone: formData.phone,
+    price: formData.price,
+  });
+
+  // Save the new Doctor document to the "doctors" collection
+  newDoctor.save((err) => {
+    if (err) {
+      console.error('Error saving Doctor:', err);
+      res.status(500).json({ message: 'Error saving Doctor' });
+    } else {
+      res.status(200).json({ message: 'Doctor saved successfully' });
+    }
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
 module.exports = doctor;
 
 
--- Frontend/src/app/form-add-doctor/form-add-doctor.component.ts
+++ Frontend/src/app/form-add-doctor/form-add-doctor.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-add-doctor.component.scss']
 })
 export class FormAddDoctorComponent {
+  formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    image: null,
+    clinicName: '',
+    location: '',
+    phoneNumber: '',
+    message: ''
+  };
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    this.http.post('https://asd-6gr1.onrender.com/doctors', this.formData).subscribe(response => {
+          console.log(response);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
+
   
 }
